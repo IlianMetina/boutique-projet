534e570c753edff2ef087076a65618d145fe4309
valjeanjean <ilian.metina83@gmail.com>
2025-06-23 23:27:10 +0200

    add prisma service & docker compose

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserServiceService } from './services/users/user.service/user.service.service';
+import { AuthServiceService } from './services/auth/auth.service/auth.service.service';
+import { PrismaServiceService } from './prisma/prisma.service/prisma.service.service';
+import { PrismaServiceService } from './prisma.service/prisma.service.service';
+import { PrismaServiceService } from './prisma/prisma.service/prisma.service.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UserServiceService, AuthServiceService, PrismaServiceService],
 })
 export class AppModule {}
