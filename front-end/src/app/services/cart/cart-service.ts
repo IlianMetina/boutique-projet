ad369fd491fbad9473e5c463af72b826d478a6e7
Ilian Metina <ilian.metina83@gmail.com>
2025-07-21 15:56:23 +0200

    adds

--- front-end/src/app/services/cart/cart-service.ts
+++ front-end/src/app/services/cart/cart-service.ts
@@ -21,14 +21,19 @@ export class CartService {
   constructor() { }
 
   private cartUrl = "http://localhost:3000/orders/";
-  private findBasketUrl = "http://localhost:3000:orders/basket/"
+  private findBasketUrl = "http://localhost:3000/orders/basket/"
 
   async getCartProducts(userId: number): Promise<Cart>{
 
     /* A changer car il nous faut un orderId pour le joueur et 
     pas son UserID pour trouver sa commande associée (status "BASKET")*/
 
-    const userOrderId = this.getOrderIdbyUser(userId);
+    const userOrderId: number = await this.getOrderIdbyUser(userId);
+
+    if(typeof userOrderId !== "number"){
+
+      throw new Error("Erreur de récupération de l'ID de la commande");
+    }
 
     const response = await fetch(this.cartUrl + userOrderId);
 
@@ -47,11 +52,20 @@ export class CartService {
     return body;
   }
 
-  async getOrderIdbyUser(userId: number){
+  async getOrderIdbyUser(userId: number): Promise<number>{
 
     const response = await fetch(this.findBasketUrl + userId);
 
+    if(!response.ok){
+      
+      throw new Error("Erreur lors de la récupération orderId");
+    }
+
     const userOrderId = await response.json();
+
+    if (typeof userOrderId !== "number") {
+      throw new Error("Erreur : l'orderId retourné n'est pas un nombre valide");
+    }
 
     return userOrderId;
   }
